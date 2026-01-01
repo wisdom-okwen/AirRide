5ad5aff52d2409c3f8381cc83164e8163432e6aa
Wisdom Okwen <wisdomokwen@gmail.com>
2026-01-01 15:29:42 -0500

    Initial Commit

---
+++ AirRide/AirRideApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirRideApp.swift
+//  AirRide
+//
+//  Created by Wisdom on 1/1/26.
+//
+
+import SwiftUI
+
+@main
+struct AirRideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirRide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AirRide
+//
+//  Created by Wisdom on 1/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
